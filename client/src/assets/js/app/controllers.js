162c7189ec0918efb5f1f84a69b99c10588dfcec
Michael Rapadas <mike@mrap.me>
2014-12-26 06:48:17 -0800

    First attempt to model keyboard layouts and lesson manager

--- client/src/assets/js/app/controllers.js
+++ client/src/assets/js/app/controllers.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var controllers = angular.module('appControllers', []);
+var controllers = angular.module('appControllers', [
+  'kbLayoutsModule'
+]);
 
-controllers.controller('TyperCtrl', function($scope, $routeParams) {
+controllers.controller('TyperCtrl', function($scope, $routeParams, lessonManager) {
   $scope.layout = $routeParams.layout;
   $scope.lesson = $routeParams.lesson;
 });
