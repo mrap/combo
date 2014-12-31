2f7265929c7b299ed67be55a28b03cbfa501863e
Michael Rapadas <mike@mrap.me>
2014-12-31 07:47:58 -0800

    Disables accidental backspace (back) navigation

---
+++ client/src/assets/js/vendor/disable-backspace-nav.js
@@ -0,0 +1,29 @@
+// Globally disables backspace button unless within a input or textarea
+// Source: http://www.technicaladvices.com/2012/07/16/preventing-backspace-from-navigating-back-in-all-the-browsers/
+(function() {
+  "use strict";
+
+  document.onkeydown = function (event) {
+
+    if (!event) { /* This will happen in IE */
+      event = window.event;
+    }
+
+    var keyCode = event.keyCode;
+
+    if (keyCode == 8 &&
+        ((event.target || event.srcElement).tagName != "TEXTAREA") &&
+          ((event.target || event.srcElement).tagName != "INPUT")) {
+
+      if (navigator.userAgent.toLowerCase().indexOf("msie") == -1) {
+        event.stopPropagation();
+      } else {
+        alert("prevented");
+        event.returnValue = false;
+      }
+
+      return false;
+    }
+  };
+
+})();
