66dbca9a39d67e785d202c96ccaad3619f3a2f15
Mike Rapadas <mike@mrap.me>
2016-12-10 01:42:29 -0800

    Try to prevent extraneous sound file requests
    This is only a temporary fix to Safari not caching audio endpoints.

--- client/src/assets/js/app/sounds.service.js
+++ client/src/assets/js/app/sounds.service.js
@@ -4,16 +4,38 @@
   angular.module('sounds.service', [])
     .service('Sounds', function() {
 
-      var keyboardPressAudio = angular.element('audio#keyboard-press');
-      this.playKeyboardPress = function() {
-        // Clone it so you can play multiple at once.
-        keyboardPressAudio.clone()[0].play();
+      function RepeatableAudioElement(id) {
+        var cloneCount = 10;
+        var audioElem = angular.element(id);
+        audioElem.autoplay = true;
+        audioElem.load();
+
+        this.audios = [];
+        for (var i = 0; i < cloneCount; i++) {
+          this.audios.push(audioElem.clone()[0]);
+        }
+
+        this.i = 0;
+      }
+
+      RepeatableAudioElement.prototype.play = function play() {
+        var a = this.audios[this.i];
+
+        // We might be trying to play a sound that's already playing.
+        if (!a.ended) {
+          a.pause();
+          a.currentTime = 0.0;
+        }
+        a.play();
+
+        if (++this.i >= this.audios.length) this.i = 0;
       };
 
-      var wrongKeyAudio = angular.element('audio#wrong-key');
-      this.playWrongKey = function() {
-        wrongKeyAudio.clone()[0].play();
-      };
+      var keyboardPressAudio = new RepeatableAudioElement('audio#keyboard-press');
+      this.playKeyboardPress = keyboardPressAudio.play.bind(keyboardPressAudio);
+
+      var wrongKeyAudio = new RepeatableAudioElement('audio#wrong-key');
+      this.playWrongKey = wrongKeyAudio.play.bind(wrongKeyAudio);
 
       var levelPassedAudio = angular.element('audio#level-passed');
       this.playLevelPassed = function() {
