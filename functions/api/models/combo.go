15ea173ec1e81cdca3a638f8438a0b0e39d87bc2
Mike Rapadas <mike@mrap.me>
2017-03-19 16:51:48 -0700

    Revert "Simplifies combo ranking"
    This reverts commit ebfe146f0eb527c77e913e5c7e2b1d19763d107a.

--- functions/api/models/combo.go
+++ functions/api/models/combo.go
@@ -5,10 +5,8 @@ import (
 	"math/rand"
 	"sort"
 
-	"golang.org/x/tools/container/intsets"
-
 	"github.com/mrap/stringutil"
-	"github.com/mrap/wordpatterns"
+	wordpatterns "github.com/mrap/wordpatterns"
 )
 
 const (
@@ -20,16 +18,12 @@ const (
 // TODO: Might provide other languages/wordlists in the future.
 //       We would need to store lookups/wordmaps in a db
 var (
-	_sharedLookup = make(wordpatterns.Wordmap)
+	_sharedLookup  = make(wordpatterns.Wordmap)
+	_sharedWordmap = make(wordpatterns.Wordmap)
 )
 
 func init() {
 	StoreWords("wordlists/google-10000-english.txt")
-}
-
-type Combo struct {
-	S     string
-	Count int
 }
 
 func GenerateCombos(chars string, count, minLen, maxLen int) []string {
@@ -54,14 +48,15 @@ func GenerateCombos(chars string, count, minLen, maxLen int) []string {
 		maxLen = minLen
 	}
 
-	combos := SortedCombos(_sharedLookup, chars, minLen, maxLen)
-	return generateComboList(combos, count)
+	combos := RankedCombos(_sharedLookup, chars, minLen, maxLen)
+	return generateComboList(_sharedWordmap, combos, count)
 }
 
 // StoreWords stores words from a word list file into
 func StoreWords(filename string) {
 	wm := wordpatterns.CreateWordmap(filename)
-	_sharedLookup = CreateCharWordLookup(wm)
+	_sharedWordmap = wm
+	_sharedLookup = CreateCharWordLookup(_sharedWordmap)
 }
 
 // CreateCharWordLookup creates a lookup structure that makes it efficient
@@ -79,71 +74,63 @@ func CreateCharWordLookup(wm wordpatterns.Wordmap) wordpatterns.Wordmap {
 	return lookup
 }
 
-func CombosWithChars(lookup wordpatterns.Wordmap, chars string, minLen int, maxLen int) []Combo {
+func CombosWithChars(lookup wordpatterns.Wordmap, chars string, minLen int, maxLen int) []string {
 	if maxLen < 0 {
-		maxLen = intsets.MaxInt
+		maxLen = 0
 	}
 	if minLen < 0 {
 		minLen = 0
 	}
 
-	var combos []Combo
-	for _, sub := range sortedSubstrs(chars, minLen) {
-		if (len(sub) >= minLen) && (len(sub) <= maxLen) {
-			combos = append(combos, Combo{sub, len(lookup[sub])})
+	var combos []string
+	sortedChars := stringutil.SortString(chars)
+
+	for _, subsorted := range stringutil.Substrs(sortedChars, 2) {
+		if (minLen != 0 && len(subsorted) < minLen) || (maxLen != 0 && len(subsorted) > maxLen) {
+			continue
+		} else {
+			combos = append(combos, lookup[subsorted]...)
 		}
 	}
 	return combos
 }
 
-func sortedSubstrs(chars string, minLen int) []string {
-	sortedChars := stringutil.SortString(chars)
-	return stringutil.Substrs(sortedChars, minLen)
-}
-
-// SortedCombos returns combos ranked by most occuring in the wordmap to least.
-// i.e. a combo is more occurant if it is a substring of more words in the wordmap.
-func SortedCombos(lookup wordpatterns.Wordmap, chars string, minLen, maxLen int) []Combo {
+func RankedCombos(lookup wordpatterns.Wordmap, chars string, minLen, maxLen int) []string {
 	combos := CombosWithChars(lookup, chars, minLen, maxLen)
-	sort.Sort(ByCount(combos))
+	sort.Sort(ByWordCount(combos))
 	return combos
 }
 
 // generateComboList returns a slice of combos that are distributed based on rank
 // More occurring combos will appear more frequently
-func generateComboList(combos []Combo, count int) []string {
-	matchCount := sumCount(combos)
+func generateComboList(wm wordpatterns.Wordmap, combos []string, count int) []string {
+	total := 0
+	for _, c := range combos {
+		total += len(wm[c])
+	}
+
 	list := make([]string, count)
+	var r, cur int
 	for i := 0; i < count; i++ {
-		list[i] = probableCombo(combos, matchCount).S
+		cur = 0
+		r = rand.Intn(total) + 1
+		for _, c := range combos {
+			cur += len(wm[c])
+			if r <= cur {
+				list[i] = c
+				break
+			}
+		}
 	}
 	return list
 }
 
-func sumCount(combos []Combo) int {
-	total := 0
-	for _, c := range combos {
-		total += c.Count
-	}
-	return total
-}
+type ByWordCount []string
 
-// probableCombo returns probable combo based on occurances.
-// i.e. the higher the combo count, the more likely to chose it.
-func probableCombo(sortedCombos []Combo, totalMatches int) Combo {
-	i := 0
-	for rem := rand.Intn(totalMatches) + 1; rem > 0; i++ {
-		rem -= sortedCombos[i].Count
-	}
-	return sortedCombos[i]
-}
-
-type ByCount []Combo
-
-func (a ByCount) Len() int      { return len(a) }
-func (a ByCount) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByCount) Less(i, j int) bool {
-	return a[i].Count < a[j].Count
+func (a ByWordCount) Len() int      { return len(a) }
+func (a ByWordCount) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByWordCount) Less(i, j int) bool {
+	return _sharedWordmap.Compare(a[i], a[j]) >= 0
 }
 
 func printComboListCounts(combos []string) {
